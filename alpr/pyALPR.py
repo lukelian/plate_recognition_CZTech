5f2c2390438cffc9dcb71c8fd22eb0c5b40a2c96
nlcc0704wstclh <769432064@qq.com>
2018-10-29 02:42:03 +0800

    2018.10.29 v6

--- alpr/pyALPR.py
+++ alpr/pyALPR.py
@@ -1,4 +1,6 @@
 import json, shlex, subprocess
+import sys
+from openalpr import Alpr
 
 class PlateReader:
 
@@ -62,7 +64,34 @@ class PlateReader:
                 print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
                 # print ("Guess {0:d}: {1:s} {2:.2f}%".format(ordinal, candidate["plate"], candidate["confidence"]))
 
+    def read_plate_py(self):
+        self.webcam_subprocess().communicate()
+        alpr = Alpr("eu", "/home/pi/lukelian/plate_recognition_CZTech/openalpr.conf",
+                    "/home/pi/lukelian/plate_recognition_CZTech/runtime_data")
+        if not alpr.is_loaded():
+            print("Error loading OpenALPR")
+            sys.exit(1)
+
+        alpr.set_top_n(20)
+        alpr.set_default_region("hr")
+        results = alpr.recognize_file("alpr.jpg")
+        i = 0
+        for plate in results['results']:
+            i += 1
+            print("Plate #%d" % i)
+            print("   %12s %12s" % ("Plate", "Confidence"))
+            for candidate in plate['candidates']:
+                prefix = "-"
+                if candidate['matches_template']:
+                    prefix = "*"
+                print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
+
+        # Call when completely done to release memory
+        alpr.unload()
+
+
+
 
 if __name__=="__main__":
     plate_reader = PlateReader()
-    plate_reader.read_plate()
+    plate_reader.read_plate_py()
